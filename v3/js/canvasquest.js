507b49b606bb18ebcf9860819019f070390a43b2
Andrew Wooldridge <triptych@whisperstorm.com>
2015-11-23 04:43:47 +0000

    Starting with PhaserJS 2.4.4

--- v3/js/canvasquest.js
+++ v3/js/canvasquest.js
@@ -1,4 +1,31 @@
-YUI().use("node", function(Y){
-	Y.log("Hello World");
+var gameProperties = {
+    screenWidth: 640,
+    screenHeight: 480,
+};
 
-});
+var states = {
+    game: "game",
+};
+
+var gameState = function(game){
+
+};
+
+gameState.prototype = {
+
+    preload: function () {
+        console.log("gamestate: preload");
+    },
+
+    create: function () {
+        console.log("gamestate: create");
+    },
+
+    update: function () {
+        console.log("gamestate: update");
+    },
+};
+
+var game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHeight, Phaser.AUTO, 'canvasQuest');
+game.state.add(states.game, gameState);
+game.state.start(states.game);
